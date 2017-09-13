63abfaca4450ba5e4a086bda11f820d17d2eb87f
ivanmlaborda <ivanmlaborda@gmail.com>
2017-09-13 18:32:03 +0200

    overlays ok

--- client/js/mapping-scripts.js
+++ client/js/mapping-scripts.js
@@ -1,26 +1,27 @@
 // Map
 const map = L.map('map', {
   center: [41.249359, 1.602561],
-  zoom: 15,
+  zoom: 16,
   minZoom: 3,
   maxZoom: 18
 })
 
 const baseHydda = L.tileLayer('https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
-	maxZoom: 18,
-	attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  maxZoom: 18,
+  attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 })
+baseHydda.addTo(map)
 
 const layer01 = L.tileLayer('data/google_tiles_01/{z}/{x}/{y}.png', {
   tms: true,
   opacity: 0.8,
-  attribution: "aerial data"
+  attribution: 'aerial data'
 })
 
 const layer02 = L.tileLayer('data/google_tiles_02/{z}/{x}/{y}.png', {
   tms: true,
   opacity: 0.8,
-  attribution: "aerial data"
+  attribution: 'aerial data'
 })
 
 const baseMaps = {
@@ -33,7 +34,8 @@ const overlays = {
 }
 
 const controlLayersOptions = {
-  position: 'topright'
+  position: 'topright',
+  hideSingleBase: true
 }
 
 L.control.layers(baseMaps, overlays, controlLayersOptions).addTo(map)
