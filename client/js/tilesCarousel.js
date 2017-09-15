72d6d906ed32fb0776c903f2b7b11f4d9c50f86c
ivanmlaborda <ivanmlaborda@gmail.com>
2017-09-15 14:32:55 +0200

    plugin

--- client/js/tilesCarousel.js
+++ client/js/tilesCarousel.js
@@ -20,26 +20,3 @@ const tilesCarousel = (...lyrs) => {
     })
   })
 }
-
-
-
-
-
-
-
-// const tilesCarousel = () => {
-//   let layers = {}
-//   let activeLayer = ''
-//   layers.layer01 = layer01
-//   layers.layer02 = layer02
-//   activeLayer = layer01
-//   layers.layer01.addTo(map)
-//   $(document).ready(() => {
-//     $('#carouselTiles').on('slide.bs.carousel', (e) => {
-//       let tileId = e.relatedTarget.id
-//       map.removeLayer(activeLayer)
-//       activeLayer = layers[tileId]
-//       activeLayer.addTo(map)
-//     })
-//   })
-// }
